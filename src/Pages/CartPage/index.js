3f951cdb329faa94ef977040c58e76716b861969
seegeegaja <wnsdyd821@gmail.com>
2025-03-21 14:48:01 +0900

    asdasdas

--- src/Pages/CartPage/index.js
+++ src/Pages/CartPage/index.js
@@ -1,20 +1,18 @@
 import Button from 'react-bootstrap/Button';
 import './cart.css'
 import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUserData } from '../../Components/reducers/user/userThunk';
 import axios from 'axios';
-
+import { data } from 'react-router-dom';
+import axiosInstance from '../../Token/Token';
 
 function CartPage(){
-  const [cartList , setCartList] = useState([])
-  useEffect(()=>{
-    axios.get("http://localhost:8081/cart/find")
-    .then((response)=>{
-      setCartList(response.data)
-    })
-    .catch((error)=>{
-      console.log("this error")
-    })
-  },[])
+  const [cartList , setCartList] = useState([]);
+  const dispatch = useDispatch();
+  
+ 
+  
 
   return(
     <div>
@@ -22,18 +20,18 @@ function CartPage(){
       {cartList.map((cartList , index)=>(
         <div className="cartDiv_box" key={index}> 
         <div className='cartImg_box'>
-          <img className="cartImg" src={cartList.postReqDto.imageUrls} ></img>
-          <h1>{cartList.postRedDto.title}</h1>
+          <img className="cartImg" src='' ></img>
+          <h1>sad</h1>
         </div>
         <div className='cartInfo_box'>
           <div>
-          <p>OnePiece PRICE : {cartList.postRedDto.price}</p>
+          <p>OnePiece PRICE : Title</p>
           </div>
           <div>
           <p>stock  <span><Button variant="outline-dark">-</Button></span><span><Button variant="outline-dark">+</Button></span> <span><Button variant="outline-dark">X</Button></span></p>
           </div>
           <div>
-          <p>Total PRICE : {cartList.price}</p>
+          <p>Total PRICE : price</p>
           </div>
         </div>
        </div>
