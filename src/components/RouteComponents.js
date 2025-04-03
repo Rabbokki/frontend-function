ef61d4e90ed4e4b54feb9668226fcfa9b963ee8d
Rabbokki <j55267035@gmail.com>
2025-04-03 17:19:21 +0900

    ㅇㅇㅇㅇㅇㅈㄷㅂㅈㄷ

--- src/components/RouteComponents.js
+++ src/components/RouteComponents.js
@@ -1,7 +1,7 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Whiskey from "../pages/alcohol/Whiskey";
+import Whiskey from "../pages/alcohol/Whiskey/index";
 import { Routes, Route } from "react-router-dom";
 import Authenticate from "../pages/authenticate/authenticate";
 import Callback from "../pages/authenticate/Callback";
